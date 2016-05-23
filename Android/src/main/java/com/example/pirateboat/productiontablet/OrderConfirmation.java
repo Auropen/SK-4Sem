0eee00810b08aa85a376d91a7a87e1d989740a4d
Swodah <swodah.main@gmail.com>
2016-05-23 13:43:45 +0200

    Android OO

---
+++ Android/src/main/java/com/example/pirateboat/productiontablet/OrderConfirmation.java
@@ -0,0 +1,13 @@
+package com.example.pirateboat.productiontablet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OrderConfirmation extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_confirmation);
+    }
+}
