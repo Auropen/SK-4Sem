abb3120cb9568d7a6e4e994baa929e57698a3d20
Auropen <sonixbock@hotmail.com>
2016-05-23 13:45:12 +0200

    Separated the server from the client :dragon_face:

---
+++ C#/SKServer/Server/Program.cs
@@ -0,0 +1,23 @@
+﻿using Server.domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            SKServer s = new SKServer();
+            while (true)
+            {
+                Console.WriteLine("Testing: " + i++);
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ C#/SKServer/Server/domain/SKServer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.domain
+{
+    class SKServer
+    {
+         
+        public SKServer()
+        {
+            string ip = ConfigurationManager.AppSettings["ip"];
+            int port = Convert.ToInt32(ConfigurationManager.AppSettings["port"]);
+            Console.WriteLine(ip + ":" + port);
+        }
+    }
+}
