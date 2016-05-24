d10a313e6127291a927ed0a1b7194ff4984748be
Auropen <sonixbock@hotmail.com>
2016-05-24 13:38:59 +0200

    Server connection WIP :dragon_face:

---
+++ C#/SKProject/SKOffice/Domain/Connection/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKOffice.Domain.Connection
+{
+    public class Client
+    {
+        public Client()
+        {
+
+        }
+    }
+}
--- C#/SKProject/SKOffice/gui/MainForm.cs
+++ C#/SKProject/SKOffice/gui/MainForm.cs
@@ -1,12 +1,4 @@
-﻿using SKOffice.domain.order;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
 
 namespace SKOffice
@@ -20,9 +12,7 @@ namespace SKOffice
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            OrderParser OP = OrderParser.Instance;
-            OrderConfirmation oc = OP.readOrder("C:\\School\\SKøkken\\w0000520.e02");
-            Console.WriteLine("Done..");
+
         }
     }
 }
